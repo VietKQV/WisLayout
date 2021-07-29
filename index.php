4a69a19a6f38925b495f05f706561b4410dc81f0
VietKq <khuongviet11032000@gmail.com>
2021-07-29 22:01:20 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.5.0/css/all.min.css" />
+  <title>Index</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+  <div class="header">
+    <i class="fas fa-cog" ></i>
+    <h1>Undeviating</h1>
+  </div>
+
+  <div class="topnav" >
+    <ul style="display: flex; list-style: none; max-width: 709px; width: 100%; margin: auto;">
+      <li>
+        <a href="#" >HOMEPAGE</a>
+      </li>
+      <li>
+       <a href="#">OUR CLIENTS</a>
+     </li>
+     <li>
+      <a href="#">ABOUT US</a>
+    </li>
+    <li>
+     <a href="#">CAREERS</a>
+   </li>
+   <li>
+     <a href="#">CONTACT US</a>
+   </li>
+ </ul> 
+</div>
+
+<div class="row">
+  <div class="column side">
+
+  </div>
+
+  <div class="column middle">
+    <img src="images/main_img.png" style="width:100%;height: 100%;">
+    <h1>Welcome to our website</h1>
+    <p>This is <b>Undevaiting</b>, a free, fully standards-compitant CSS template designed by <a href="#">TEMPLATED</a>. The photos in this template are from <a href="#"> Fotograph</a> </p>
+    <p>this free template is released under the <a href="#"> Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it</p>
+    <p>(even use it commercially) provied you give us credit for it. Have fun :)</p>
+  </div>
+
+  <div class="column side">
+
+  </div>
+</div>
+
+<div>
+ <div style="background: #E3F0F7; max-width: 1900px; width: 100%;">
+ <div class="footer" >
+
+  <div class="rows">
+    <div class="column  ">
+      <i class="fas fa-map-marker"></i>
+      <p>1234 Fictitonnal Road Suite #789</p>
+      <p>Nashville TN 0000</p>
+    </div>    
+  </div>
+
+
+  <div class="rows">
+    <div class="column ">
+      <i class="fas fa-phone"></i>
+      <p>Telephone: +1800 1234567</p>
+    </div>    
+  </div>
+
+
+  <div class="rows">
+    <div class="column ">
+      <i class="fas fa-envelope"></i>
+      <p>bussinessname@gmail.com</p>
+    </div>    
+  </div> 
+
+</div>
+
+<div style="background : #2056ac; max-width: 1900px; width: 100%;">
+  <div class="footer_bottom">
+  <p> Untitled. All rights reserved.| Photos by <b>Fotograph</b> | Design by <b>TEMPLATED</b>. </p>
+
+  <ul style="display:flex; list-style: none;">
+    <li>
+      <a href="#"><img src="images/1.png"></a>
+    </li>
+    <li>
+      <a href="#"><img src="images/1.png"></a>
+    </li>
+    <li>
+      <a href="#"><img src="images/1.png"></a>
+    </li>
+    <li>
+      <a href="#"><img src="images/1.png"></a>
+    </li>
+    <li>
+      <a href="#"><img src="images/1.png"></a>
+    </li>
+  </ul>
+</div>
+</div>
+
+
+</div>
+</div>
+
+
+</body>
+</html>
